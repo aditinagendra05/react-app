60567cf2880cfc92fe918d0f74b1c26db9d747ae
aditinagendra05 <aditinagendra05@gmail.com>
2026-01-20 20:15:48 +0530

    rendering a list

--- practice-react-app/src/components/ListGroup.tsx
+++ practice-react-app/src/components/ListGroup.tsx
@@ -1,15 +1,14 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  const city=["Delhi","Mumbai","Bangalore","Chennai","Kolkata"];
+  
   return (
     <Fragment>
         <h1>List</h1>
         <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
+          {city.map(item=><li key={item}>{item}</li>)} 
+
         </ul>
     </Fragment>
   );
